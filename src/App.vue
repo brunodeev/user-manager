d06374a43922a0dc76a2a0ca0e81193945e53ffb
Bruno César <bcgmeireles@gmail.com>
2024-05-11 00:02:06 -0300

    feat: implements form css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
-<template>
-  <form>
-    <input type="text" v-model="user.name">
-    <input type="number" v-model="user.age">
-    <button v-on:click="addUser">Adicionar</button>
-  </form>
-
-  <ul>
-    <li v-for="user in users">{{ user.name }} - {{ user.age }}</li>
-  </ul>
-</template>
-
 <script>
 export default {
   data() {
@@ -46,3 +34,77 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="container">
+    <form class="forms">
+      <div class="inputs">
+        <label for="input-name">Nome</label>
+        <input class="input-container" id="input-name" type="text" v-model="user.name">
+      </div>
+      <div class="inputs">
+        <label for="input-age">Idade</label>
+        <input class="input-container" id="input-age" type="number" v-model="user.age">
+      </div>
+      <button class="input-button" v-on:click="addUser">Adicionar</button>
+    </form>
+
+    <ul class="list">
+      <li class="item-list" v-for="user in users">{{ user.name }} - {{ user.age }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Ubuntu Sans', sans-serif;
+}
+
+.container {
+  background-color: #2e2e2e;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.forms {
+  background-color: #f6f6f6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 20rem;
+  width: 25rem;
+  padding: 20px;
+  border-radius: 12px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, .5);
+}
+
+.inputs {
+  display: flex;
+  flex-direction: column
+}
+
+.inputs label {
+  line-height: 1.8;
+}
+
+.input-container {
+  height: 2.5rem;
+  border: none;
+  border-radius: 8px;
+  padding-left: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+}
+
+.input-button {
+  height: 2.5rem;
+  border: none;
+  border-radius: 8px;
+  background-color: #28cb61;
+  cursor: pointer;
+}
+</style>
